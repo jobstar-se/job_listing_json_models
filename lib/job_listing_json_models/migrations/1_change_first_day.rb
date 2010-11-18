1ba72eba779350d8ffdc2670f997d096a2bed0a4
pch <piotr@chmolowski.pl>
2010-11-18 11:59:21 +0100

    Use convert! instead of convert

--- lib/job_listing_json_models/migrations/1_change_first_day.rb
+++ lib/job_listing_json_models/migrations/1_change_first_day.rb
@@ -1,6 +1,6 @@
 class ChangeFirstDay < JobListingJsonModels::Version
   def self.migrate(json_hash)
-    HashConverter.convert(json_hash) do
+    HashConverter.convert!(json_hash) do
       map "first_day", "duration.starts_on"
     end
   end
--- spec/json_model_spec.rb
+++ spec/json_model_spec.rb
@@ -6,6 +6,8 @@ describe JsonModel do
     json = JobListingJsonModels::Base.from_json("{}")
 
     json = json.to_json_version(1)
-    json.should == ""
+    json = ActiveSupport::JSON.decode(json)
+
+    json.should == {"requirements"=>{"drivers_license"=>{"types"=>0}}, "worktime"=>{"types"=>0}, "seasons"=>0, "num_positions"=>1, "locations"=>[], "categories"=>[], "contacts"=>[], "union_contacts"=>[], "duration"=>{"starts_on"=>nil}}
   end
 end
