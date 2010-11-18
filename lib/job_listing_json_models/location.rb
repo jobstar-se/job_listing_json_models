5bb06cf0474c0b9a39eb0e194123ec94f6b3a618
Peder Linder <pederbl@jobstar.se>
2010-11-18 11:37:03 +0000

    make location.id required and change from string to integer

--- lib/job_listing_json_models/location.rb
+++ lib/job_listing_json_models/location.rb
@@ -8,13 +8,15 @@ module JobListingJsonModels
     CITY      = 3
     CITY_AREA = 4
     
-    attribute :level,   String # The level of specificity of the location
-    attribute :id,      String # The JobStar.se code of the location
+    attribute :id,      Integer # The JobStar.se code of the location
     attribute :address, String # The visit address of the work location
+    attribute :at_home, Boolean
+
     attribute :city,    String # Temporary hack until we have location mapping for all sources
     attribute :region,  String 
     attribute :country, String
-    attribute :at_home, Boolean
+
+    validates :id, :presence => true
   end
 
 end
